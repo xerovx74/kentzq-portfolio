63784f73bb7235e55127023abe93f86b3fe231cf
DDR13GIT <ddroy13@gmail.com>
2022-05-27 23:59:09 +0600

    init

--- pages/guestbook.tsx
+++ pages/guestbook.tsx
@@ -1,45 +1,64 @@
-import prisma from 'lib/prisma';
 import Container from 'components/Container';
 import Guestbook from 'components/Guestbook';
 
 export default function GuestbookPage({ fallbackData }) {
   return (
-    <Container
-      title="Guestbook – Lee Robinson"
-      description="Sign my digital guestbook and share some wisdom."
-    >
-      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-          Guestbook
-        </h1>
+    <Container>
+      <div className="flex flex-col justify-center items-start max-w-4xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
+        <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-black dark:text-white">
+          Featured Posts
+        </h3>
+        {/* <div className="flex gap-6 flex-col md:flex-row">
+          <BlogPostCard
+            title="Everything I Know About Style Guides, Design Systems, and Component Libraries"
+            slug="style-guides-component-libraries-design-systems"
+            gradient="from-[#D8B4FE] to-[#818CF8]"
+          />
+          <BlogPostCard
+            title="Rust Is The Future of JavaScript Infrastructure"
+            slug="rust"
+            gradient="from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]"
+          />
+          <BlogPostCard
+            title="Past, Present, and Future of React State Management"
+            slug="react-state-management"
+            gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
+          />
+        </div> */}
+
+        <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-16 text-black dark:text-white">
+          Learn React & Next.js
+        </h3>
         <p className="text-gray-600 dark:text-gray-400 mb-4">
-          Leave a comment below. It could be anything – appreciation,
-          information, wisdom, or even humor. Surprise me!
+          Build and deploy a modern SaaS application using the most popular
+          open-source software. This course is 12 hours long and is completely
+          live streamed.
         </p>
-        <Guestbook fallbackData={fallbackData} />
+
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.youtube.com/playlist?list=PL6bwFJ82M6FXgctyoWXqj7H0GK8_YIeF1"
+          className="flex mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6"
+        >
+          Watch all videos
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="h-6 w-6 ml-1"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
+            />
+          </svg>
+        </a>
+        <span className="h-16" />
       </div>
     </Container>
   );
-}
-
-export async function getStaticProps() {
-  const entries = await prisma.guestbook.findMany({
-    orderBy: {
-      updated_at: 'desc'
-    }
-  });
-
-  const fallbackData = entries.map((entry) => ({
-    id: entry.id.toString(),
-    body: entry.body,
-    created_by: entry.created_by.toString(),
-    updated_at: entry.updated_at.toString()
-  }));
-
-  return {
-    props: {
-      fallbackData
-    },
-    revalidate: 60
-  };
 }
